d0549d79c514ce16ac1d21ba9093cc38d8e7a1fe
Wei Zhe Liu <70379927+oxcitzen@users.noreply.github.com>
2024-09-16 20:32:34 +0800

    Add files via upload

---
+++ region_of_interest.py
@@ -0,0 +1,58 @@
+import cv2
+
+go_flag = 0
+
+def mouse_click(event, x, y, flags, params):
+    global x1, y1, x2, y2
+    global go_flag
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x1 = x
+        y1 = y
+        go_flag = 0
+            
+    if event == cv2.EVENT_LBUTTONUP:
+        x2 = x
+        y2 = y
+        go_flag = 1
+         
+        
+
+cv2.namedWindow('nanoCam')
+cv2.setMouseCallback('nanoCam', mouse_click)
+dispW=640
+dispH=480
+flip=2
+
+#Un omment These next Two Line for Pi Camera
+#camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+#cam= cv2.VideoCapture(camSet)
+
+
+cam=cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('nanoCam',frame)
+    
+    if go_flag == 1:
+        frame == cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        
+        if y1<y2 and x1<x2:
+            roi = frame[y1:y2,x1:x2]  #right bottom
+        if y1<y2 and x2<x1:
+            roi = frame[y1:y2,x2:x1]  #left bottom
+        if y2<y1 and x1<x2:
+            roi = frame[y2:y1,x1:x2]   #right up    
+        if y2<y1 and x2<x1:
+            roi = frame[y2:y1,x2:x1]   #left up
+            
+        cv2.imshow('Copy_ROI', roi)
+        
+    cv2.moveWindow('nanoCam', 0, 0)
+    
+    if cv2.waitKey(1)==ord('q'):
+        break
+    
+cam.release()
+cv2.destroyAllWindows()
---
+++ threshold_mask.py
@@ -0,0 +1,93 @@
+import cv2
+
+dispW=320
+dispH=240
+flip=2
+
+def nothing(x):
+    pass
+
+# Create the 'blended' window and a trackbar for blending
+cv2.namedWindow('blended')
+cv2.createTrackbar('blend_v', 'blended', 50, 100, nothing)
+
+cv_logo = cv2.imread('mask.jpg') 
+cv_logo = cv2.resize(cv_logo, (320, 240))
+
+# To do thresholing with gray scale 
+cv_logo_gray = cv2.cvtColor(cv_logo, cv2.COLOR_BGR2GRAY) 
+cv2.imshow('cv_logo_gray', cv_logo_gray)
+cv2.moveWindow('cv_logo_gray', 0, 720)
+
+# 255 white, 0 black
+# 220 is the thresh value. Pixel below is set to 0 and pixel above is set to max value
+# 255 is the max value
+_, bg_mask = cv2.threshold(cv_logo_gray, 220, 255, cv2.THRESH_BINARY)
+cv2.imshow('BG_mask', bg_mask)
+cv2.moveWindow('BG_mask', 330, 440)
+
+# The inversion of bg_mask 
+fg_mask = cv2.bitwise_not(bg_mask)
+cv2.imshow('FG_mask', fg_mask)
+cv2.moveWindow('FG_mask', 330, 720)
+
+
+# mask 
+# Pixels in the mask that are non-zero will have the operation applied to the corresponding pixels in the source image. 
+# Pixels in the mask that are zero (black, i.e., 0) will not be affected.  
+fg = cv2.bitwise_and(cv_logo , cv_logo, mask=fg_mask)
+cv2.imshow('FG', fg)
+cv2.moveWindow('FG', 660, 720)
+
+
+
+#Uncomment These next Two Line for Pi Camera
+#camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+#cam= cv2.VideoCapture(camSet)
+
+cam=cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    
+    frame = cv2.resize(frame, (dispW, dispH))
+    
+    cv2.namedWindow('nanoCam', cv2.WINDOW_NORMAL)
+    cv2.imshow('nanoCam',frame)
+    cv2.moveWindow('nanoCam', 0, 440)
+    
+    # mask 
+    # Pixels in the mask that are non-zero will have the operation applied to the corresponding pixels in the source image. 
+    # Pixels in the mask that are zero (black, i.e., 0) will not be affected.
+    bg = cv2.bitwise_and(frame, frame, mask=bg_mask)
+    cv2.imshow('BG', bg)
+    cv2.moveWindow('BG', 660, 440)
+    
+    
+    comp_image = cv2.add(bg, fg)
+    cv2.imshow('comp_image', comp_image)
+    cv2.moveWindow('comp_image', 990, 440)
+    
+    # get the value from the track bar
+    bv1 = cv2.getTrackbarPos('blend_v', 'blended') / 100
+    bv2 = 1 - bv1
+    
+    blended = cv2.addWeighted(frame, bv1, cv_logo, bv2, 0)
+    cv2.imshow('blended', blended)
+    cv2.moveWindow('blended', 990, 720)
+    
+    # Pixels in the mask that are zero (black, i.e., 0) will not be affected.
+    fore_ground_2 = cv2.bitwise_and(blended , blended, mask=fg_mask)
+    cv2.imshow('FG2', fore_ground_2)
+    cv2.moveWindow('FG2', 1320, 440)
+    
+    #comp_final = cv2.addWeighted(frame, 1, fore_ground_2, 2, 2)
+    comp_final = cv2.add(bg, fore_ground_2)
+    cv2.imshow('comp_final', comp_final)
+    cv2.moveWindow('comp_final', 1320, 720)
+    
+    
+    if cv2.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
